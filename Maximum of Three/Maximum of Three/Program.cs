38166ab501a836c36e15b2c78decf2f0d4fe7ff6
DilanRaza1 <dilan.raza@iths.se>
2024-08-28 17:38:17 +0200

    Maximum of Three

---
+++ Maximum of Three/Maximum of Three/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+int num1;
+int num2;
+int num3;
+
+Console.WriteLine("Input first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input third number: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine("the maximum number is " + num1);
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine("The maxiumum number is " + num2);
+}
+else Console.WriteLine("The maximum number is " + num3);
