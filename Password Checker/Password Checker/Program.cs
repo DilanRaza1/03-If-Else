f484b41a37c8f92cc08cab8f21b6e880ebf66dba
DilanRaza1 <dilan.raza@iths.se>
2024-08-28 19:56:37 +0200

    Password Checker

---
+++ Password Checker/Password Checker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+string password = "Bananfisk123";
+string inputPassword;
+Console.WriteLine("input a password: ");
+inputPassword = Console.ReadLine();
+
+if (inputPassword == password)
+{
+    Console.WriteLine("Access granted, Welcome!");
+}
+else
+{
+    Console.WriteLine("Wrong password, try again.");
+}
